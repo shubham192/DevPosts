485ffd49a9e341786384097ba5bda38cd13aa0da
Andre Jarrell <mail@andrejarrell.com>
2020-08-15 21:02:39 +1000

    check url

--- action/https.js
+++ action/https.js
@@ -2,6 +2,7 @@ let https = require('https');
 
 module.exports.get = url => {
     return new Promise((resolve, reject) => {
+        console.log(url);
         https.get(url, response => {
             let data = '';
             response.on('data', chunk => {
